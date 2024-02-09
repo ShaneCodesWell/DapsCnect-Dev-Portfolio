f859cb4bd94ed557188947a36fe6ff27cf7f8df3
Shane Mensah <shanecodes69@gmail.com>
2024-02-09 11:00:40 +0000

    cleaning up console errors

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,14 +4,14 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">{technologies.map((technology) => (
-      <div className="w-28 h-28 key={technology.name}">
-        <BallCanvas icon={technology.icon} />
-      </div>
-    ))}
-
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div key={technology.name} className="w-28 h-28">
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Tech, "");
